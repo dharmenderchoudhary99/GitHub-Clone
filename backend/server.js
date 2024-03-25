9ed7cf225548374cb105ad43c08106a77a9fa125
dharmenderchoudhary99 <dharmenderchoudhary022@gmail.com>
2024-03-25 11:33:05 +0530

    Adding the Backend to server with Home Page functionality

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import dotenv from "dotenv"
+import cors from "cors"
+
+import userRoutes from "./routes/user.route.js"
+import exploreRoutes from "./routes/explore.route.js"
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+// Time:2:25:00
+
+app.get("/",(req,res)=>{
+    res.send("Server is ready")
+});
+
+app.use("/api/users",userRoutes)
+app.use("/api/explore",exploreRoutes);
+
+app.listen(5000,()=>{
+    console.log("Server started on port:5000")
+})
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -19,18 +19,13 @@ const HomePage = () => {
     async (username = "dharmenderchoudhary99") => {
       setLoading(true);
       try {
-        const userRes = await fetch(`https://api.github.com/users/${username}`,{
-          headers:{
-            authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`
-          }
-        });
-        const userProfile = await userRes.json();
-        setUserProfile(userProfile);
-
-        const repoRes = await fetch(userProfile.repos_url);
-        const repos = await repoRes.json();
-        repos.sort((a,b)=> new Date(b.created_at)-new Date(a.created_at));
+        const res = await fetch(`http://localhost:5000/api/users/profile/${username}`);
+        const {repos, userProfile} = await res.json();
+        
+        repos.sort((a,b)=> new Date(b.created_at) - new Date(a.created_at));
+        
         setRepos(repos);
+        setUserProfile(userProfile);
 
         console.log("userProfile", userProfile);
         console.log("repos:", repos);
